f27d33420c62e157cec264eb3442417fe8de6db1
Ugochukwu Ekwueme <ekwuemeugochukwu83@gmail.com>
2025-09-18 11:57:52 +0100

    Add unit tests for CourseController and update TestUtil for CourseDTO

---
+++ src/test/unit/com/kotlinspring/controllers/CourseControllerUnitTest.kt
@@ -0,0 +1,115 @@
+package com.kotlinspring.controllers
+
+import com.kotlinspring.dto.CourseDTO
+import com.kotlinspring.entity.Course
+import com.kotlinspring.services.CourseService
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.reactive.server.WebTestClient
+import com.kotlinspring.util.courseDTO
+
+
+@WebMvcTest(controllers = [CourseController::class])
+@AutoConfigureWebTestClient
+class CourseControllerUnitTest {
+
+    @Autowired
+    lateinit var webTestClient: WebTestClient
+
+    @MockkBean
+    lateinit var courseServiceMockk: CourseService
+
+    @Test
+    internal fun addCourse() {
+        val courseDTO = CourseDTO(
+            id = null,
+            name = "Kotlin Spring Boot",
+            category = "Programming Course"
+        )
+
+        every { courseServiceMockk.addCourse(any()) } returns courseDTO(id = 1)
+
+        val savedCourseDTO = webTestClient.post()
+            .uri("/v1/courses")
+            .bodyValue(courseDTO)
+            .exchange()
+            .expectStatus().isCreated
+            .expectBody(CourseDTO::class.java)
+            .returnResult()
+            .responseBody
+
+        Assertions.assertTrue { savedCourseDTO!!.id != null }
+    }
+
+    @Test
+    fun retrieveAllCourses() {
+        every { courseServiceMockk.retrieveAllCourses() }.returnsMany(listOf(
+            courseDTO(id = 1),
+            courseDTO(id = 2, name = "Rest Spring Boot Course"),
+            courseDTO(id = 3),
+            courseDTO(id = 4)
+        ))
+
+        val courseDTOs = webTestClient.get()
+            .uri("/v1/courses")
+            .exchange()
+            .expectStatus().isOk
+            .expectBodyList(CourseDTO::class.java)
+            .returnResult()
+            .responseBody
+
+        println("Courses: $courseDTOs")
+        Assertions.assertEquals(4, courseDTOs!!.size)
+    }
+
+    @Test
+    fun updateCourse() {
+        every { courseServiceMockk.updateCourse(any(), any()) } returns CourseDTO(
+            id = null,
+            name = "Java Spring Boot",
+            category = "Programming Course"
+        )
+
+        //COURSE ID
+        //Updated CourseDTO
+        val newCourseDTO = CourseDTO(
+            id = null,
+            name = "Java Spring Boot - Updated",
+            category = "Programming Course - Updated"
+        )
+
+        val updatedCourse = webTestClient.put()
+            .uri("/v1/courses/{course_id}", savedCourse.id)
+            .bodyValue(newCourseDTO)
+            .exchange()
+            .expectStatus().isOk
+            .expectBody(CourseDTO::class.java)
+            .returnResult()
+            .responseBody
+
+        Assertions.assertEquals(newCourseDTO.name , updatedCourse!!.name)
+    }
+//
+//    @Test
+//    fun deleteCourse() {
+//        //existing course
+//        val course = Course(
+//            id = null,
+//            name = "Java Spring Boot",
+//            category = "Programming Course"
+//        )
+//        val savedCourse = courseRepository.save(course)
+//
+//        webTestClient.delete()
+//            .uri("/v1/courses/{course_id}", savedCourse.id)
+//            .exchange()
+//            .expectStatus().isNoContent
+//
+//        Assertions.assertFalse { courseRepository.findById(savedCourse.id!!).isPresent }
+//    }
+}
